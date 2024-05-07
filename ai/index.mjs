b8fb185aad181a1b4f945295ce88d93d424e1312
nghi.khuc <nghi.khuc@cake.vn>
2024-05-07 11:46:57 +0700

    AI

---
+++ ai/index.mjs
@@ -0,0 +1,72 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.use('/mp3', express.static(__dirname + '/mp3'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/mp3', async (req, res) => {
+  const body = req.body
+
+  // 'female_happy', 'female_sad', 'female_angry', 'female_disgust', 'female_fearful', 'female_surprised', 'male_happy', 'male_sad', 'male_angry', 'male_fearful', 'male_disgust', 'male_surprised',
+
+  const url = "https://api.play.ht/api/v2/tts/stream";
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "audio/mpeg",
+      "content-type": "application/json",
+      AUTHORIZATION: "99a1094ae424490780e837ced9d23adf",
+      "X-USER-ID": "RoXtfF4aeme6PbGv07ghs4CDgF72",
+    },
+    body: JSON.stringify({
+      voice_engine: 'PlayHT2.0-turbo',
+      text: body.text || "Hello I'm from Quokka team. This is a test product for the text to speech feature. Let's wait and see what our product brings to you",
+      voice:
+        "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
+      output_format: "mp3",
+      sample_rate: "44100",
+      speed: 1,
+    }),
+  };
+
+  const response = await fetch(url, options);
+  const readableStream = response.body;
+
+  const generateId = new Date().valueOf();
+
+  // Pipe the readable stream to a writable stream, this can be a local file or any other writable stream
+  readableStream.pipe(fs.createWriteStream(`./mp3/${generateId}.mp3`));
+  readableStream.on('finish', () => {
+    res.json({
+      ok: 1,
+      file: `${generateId}.mp3`,
+      message: `File is saved as ${generateId}.mp3`
+    })
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
