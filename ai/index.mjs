6a2d6f8fdf73705f0ee3392eff9faebdb6b97a59
nghi.khuc <nghi.khuc@cake.vn>
2024-05-07 12:20:24 +0700

    AI

--- ai/index.mjs
+++ ai/index.mjs
@@ -1,31 +1,29 @@
 import fs from "fs";
 import fetch from "node-fetch";
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
+app.use(cors());
 
-app.use('/mp3', express.static(__dirname + '/mp3'));
+app.use("/mp3", express.static(__dirname + "/mp3"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/mp3', async (req, res) => {
-  const body = req.body
-
-  // 'female_happy', 'female_sad', 'female_angry', 'female_disgust', 'female_fearful', 'female_surprised', 'male_happy', 'male_sad', 'male_angry', 'male_fearful', 'male_disgust', 'male_surprised',
+app.post("/mp3", async (req, res) => {
+  const body = req.body;
 
   const url = "https://api.play.ht/api/v2/tts/stream";
   const options = {
@@ -37,8 +35,10 @@ app.post('/mp3', async (req, res) => {
       "X-USER-ID": "RoXtfF4aeme6PbGv07ghs4CDgF72",
     },
     body: JSON.stringify({
-      voice_engine: 'PlayHT2.0-turbo',
-      text: body.text || "Hello I'm from Quokka team. This is a test product for the text to speech feature. Let's wait and see what our product brings to you",
+      voice_engine: "PlayHT2.0-turbo",
+      text:
+        body.text ||
+        "Hello I'm from Quokka team. This is a test product for the text to speech feature. Let's wait and see what our product brings to you",
       voice:
         "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
       output_format: "mp3",
@@ -53,20 +53,27 @@ app.post('/mp3', async (req, res) => {
   const generateId = new Date().valueOf();
 
   // Pipe the readable stream to a writable stream, this can be a local file or any other writable stream
-  readableStream.pipe(fs.createWriteStream(`./mp3/${generateId}.mp3`));
-  readableStream.on('finish', () => {
+  readableStream.pipe(
+    fs.createWriteStream(`./mp3/${body.file || ''}_${generateId}.mp3`)
+  );
+  readableStream.on("finish", () => {
     res.json({
       ok: 1,
       file: `${generateId}.mp3`,
-      message: `File is saved as ${generateId}.mp3`
-    })
-  })
-})
+      message: `File is saved as ${generateId}.mp3`,
+    });
+  });
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  fs.readdir(__dirname + "/mp3", (err, files) => {
+    if (err) res.json({ok: 0});
+    else {
+      res.json({ok: 1, files: files.filter(file => file.endsWith('.mp3'))})
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
